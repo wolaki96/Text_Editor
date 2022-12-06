d79bb26e8c6e23c65c5496a6fdcaeef4da21fd73
Squiddy <wolaki96@gmail.com>
2022-12-06 10:42:24 -0500

    changed folder structure

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 
 require('./routes/htmlRoutes')(app);
 
