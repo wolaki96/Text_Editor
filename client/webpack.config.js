b225028af477c02d9c36991fa1482b3996d1b562
Squiddy <wolaki96@gmail.com>
2022-12-05 19:11:58 -0500

    added mainfest

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -22,9 +22,28 @@ module.exports = () => {
       new HtmlWebpackPlugin({
         template: './index.html',
         title: 'jate'
+      }),
+      // adding manifest.json
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'Just Another Text Editor',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
       })
-      
     ],
+
 
     module: {
       rules: [
